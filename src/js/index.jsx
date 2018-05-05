ff03535eec701ea1d7a02736c704531b9ea32731
Ozgur <ozgr@live.com>
2018-05-05 18:14:30 +0300

    Add form.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -7,7 +7,19 @@ class App extends React.Component {
   render () {
     return (
       <div className='container'>
-        <h1>Hello World!</h1>
+        <div className='container2'>
+          <h2>Muny</h2>
+          <p>A todo app with <a href='https://github.com/ozgrozer/muny#resources' target='_blank'>free resources</a></p>
+
+          <form className='form' noValidate>
+            <div className='form-group'>
+              <input type='text' className='form-control form-control-lg' placeholder='Add something' />
+            </div>
+            <div className='form-group'>
+              <button type='submit' className='btn btn-primary btn-lg btn-block'>Add</button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
