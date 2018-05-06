814057059f5ec569014934b690684e1457225a99
Ozgur <ozgr@live.com>
2018-05-06 13:44:47 +0300

    Add Firebase config file.

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYRnhcuc1eT1LYYvszACo8HuFZkkI2Oz0',
+  authDomain: 'ozgrozer-muny.firebaseapp.com',
+  databaseURL: 'https://ozgrozer-muny.firebaseio.com',
+  projectId: 'ozgrozer-muny',
+  storageBucket: 'ozgrozer-muny.appspot.com',
+  messagingSenderId: '197981362707'
+}
+
+module.exports = firebaseConfig
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,16 +4,8 @@ import axios from 'axios'
 import { firebase } from '@firebase/app'
 import '@firebase/database'
 
+import firebaseConfig from './firebaseConfig'
 import './../css/style.scss'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDYRnhcuc1eT1LYYvszACo8HuFZkkI2Oz0',
-  authDomain: 'ozgrozer-muny.firebaseapp.com',
-  databaseURL: 'https://ozgrozer-muny.firebaseio.com',
-  projectId: 'ozgrozer-muny',
-  storageBucket: 'ozgrozer-muny.appspot.com',
-  messagingSenderId: '197981362707'
-}
 
 class App extends React.Component {
   constructor () {
