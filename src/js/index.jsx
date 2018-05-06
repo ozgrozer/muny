958cd0dbb71bb1d148d6195aa993b2636e21b55f
Ozgur <ozgr@live.com>
2018-05-06 11:05:20 +0300

    Use necessary Firebase plugins.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import firebase from 'firebase'
 import axios from 'axios'
+import { firebase } from '@firebase/app'
+import '@firebase/database'
 
 import './../css/style.scss'
 
